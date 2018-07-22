4c6f5bbc1e11bfecc408fc70e6bd47a0b7b401b0
benny refaelov <bennyrefaelov@gmail.com>
2018-07-22 12:57:49 +0300

    working product

--- src/test/java/piercinghunter/Main.java
+++ src/test/java/piercinghunter/Main.java
@@ -1,8 +1,11 @@
 package piercinghunter;
 
+import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -16,60 +19,102 @@ import com.mashape.unirest.http.Unirest;
 
 public class Main {
     /*
-     *  1) using api to get all stock symbols
-     *  2) get day data of each stock
-     *  3) get hourly (last hour) of each stock
+     *  1) using api to get all stock symbols done
+     *  2) get day data of each stock done
+     *  3) get hourly (last hour) of each stock - later
      *  endpoints: https://api.iextrading.com/1.0/ref-data/symbols
      * https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=YVD6NCJOTY1PF66U
+        
+        4) create txt file and put the found stocks there :)
+        
+  		5) check if peircing confirmed:
+  		
      */
-	 static ArrayList<Stock> stocks = new ArrayList<Stock>();
+	
+	 static boolean getCheckedData   = true;
+	 static ArrayList<Stock>  stocks = new ArrayList<Stock>();
 	 static ArrayList<String> stocksWithPiercing = new ArrayList<String>();
+	 static ArrayList<String> stocksWithErrors   = new ArrayList<String>();
 	 static ObjectMapper objectMapper = new ObjectMapper();
+	 static boolean piercingConfirmer = true;
+	 static boolean piercingfinder = false;
 	 
 	public static void main(String[] args) throws Exception {
+
+	    //for debuging
 //		if (checkForPiercing("XXII")) {
 //			System.out.println("We have a piercing here!!!!!");
 //		} else {
 //			System.out.println("No piercing");
 //		}
-		int requestNum = 0;
-		stocks = objectMapper
-				 .readValue(Unirest.get("https://api.iextrading.com/1.0/ref-data/symbols")
-				 .asJson()
-				 .getRawBody(),new TypeReference<List<Stock>>(){});
-		for (Stock s : stocks) {
-			if (s.type.equals("cs") || s.type.equals("et")) {
-				System.out.println(++requestNum + ". Checking " + s.symbol);
-				if (checkForPiercing(s.symbol)){
-					stocksWithPiercing.add(s.symbol);
-					System.out.println("****** piercing found:" + s.symbol + "*******");
-				} else {
-					System.out.println(s.symbol + " No Piercing");
-					if (stocksWithPiercing.size() > 0) {
-						System.out.println("piercing found on " + stocksWithPiercing.size());
-					}
-				}
-				//Thread.sleep(500); //api call restrictions
-			}		
+		if (piercingConfirmer) {
+			System.out.println("confirmer is workings");
 		}
-	
+		
+		if (piercingfinder) {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd_MM_yyyyHH_mm_ss");  
+		    Date date = new Date();  
+		    String fileName = formatter.format(date);
+		    String filePath = System.getProperty("user.dir")+"\\src\\test\\resources\\piercings\\" + fileName +".txt";
+			PrintWriter writer = new PrintWriter(filePath, "UTF-8");
+			try {
+				stocks = objectMapper
+						 .readValue(Unirest.get("https://api.iextrading.com/1.0/ref-data/symbols")
+						 .asJson()
+						 .getRawBody(),new TypeReference<List<Stock>>(){});
+			} catch (Exception e) {
+				System.exit(0);
+			}
+			
+			int requestNum = 0;
+			int foundPiercings = 0;
+			
+			String symbol;
+			for (Stock s : stocks) {
+				if (s.type.equals("cs") || s.type.equals("et")) {
+					System.out.println(++requestNum + ". Checking " + s.symbol);
+					symbol = s.symbol.replaceAll("[^A-Za-z0-9()\\[\\]]", "");
+					if (getSymbolData(symbol)){
+						stocksWithPiercing.add(symbol);
+						writer.println(++foundPiercings + " : "+symbol);
+						writer.flush();
+					} else {
+						System.out.println(symbol + " No Piercing");
+						if (stocksWithPiercing.size() > 0) {
+							System.out.println("piercing found on " + stocksWithPiercing.size());
+						}
+					}
+					//Thread.sleep(500); //api call restrictions
+				}		
+			}
+			writer.close();
+		}
+	   
 	}
 	
 	
-	private static boolean checkForPiercing(String stock) throws Exception {
-		HttpResponse<JsonNode> jsonResponse = Unirest
-		.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ stock + "&apikey=C0F6Z5D4ELEKDZ0U")
-		.asJson();
+	private static boolean getSymbolData(String stock) throws Exception {
+		HttpResponse<JsonNode> jsonResponse = null;
+		JSONObject stocks = new JSONObject();
+		try {
+			jsonResponse = Unirest
+					.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ stock + "&apikey=C0F6Z5D4ELEKDZ0U")
+					.asJson();	
+		} catch (Exception e) {
+			System.out.println("program terminated");
+			System.exit(0);
+		}
 		
 		if (jsonResponse != null && jsonResponse.getStatus() == 200) {
 			JSONObject obj = new JSONObject(jsonResponse.getBody());
 			JSONArray arr = obj.getJSONArray("array");
-			JSONObject stocks = new JSONObject();
+			
 			//System.out.println(obj);
 			try {
 				stocks = arr.getJSONObject(0).getJSONObject("Time Series (Daily)");
 			} catch(Exception exception) {
-				System.out.println("Something fucked up");
+				stocksWithErrors.add(stock);
+				System.out.println("Error here");
 				System.out.println(obj);
 				return false;
 			}
@@ -130,12 +175,10 @@ public class Main {
 			    		return false;
 			    	}
 			    } else {
-//			    	System.out.println("DownTrend, Nothing here");
-			    	return false;
+			    	return false; //"DownTrend, Nothing here"
 			    }
 		    } else {
-//		    	System.out.println("Uptrend, Nothing here");
-		    	return false;
+		    	return false; //day before uptrend
 		    }
 		} else {
 			return false;
